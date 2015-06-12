snic-nsc/clipc-dev

commit f3dfffea61bdb46a7334b412c05999400b668f77

Remove Celery worker registrar
Simplify code reasoning by removing the registrar worker and
moving the registration logic into the scheduler worker
instead. This avoids any issues that can occur mostly when a
request is being registered concurrently while the schedule_tasks
is being run.

Generate a request uuid to the user in the Flask app and defer db
registration to the scheduler worker using the requeust uuid as
the celery task id. This allows the request to be traced even
before it has been registered in the db.

src/tasks/__init__.py
@@ -30,8 +30,7 @@ def update_sent_state(sender=None, body=None, **kwargs):
 
 
 cel = celery.Celery('soda',
-                    include=['tasks.registrar',
-                             'tasks.scheduler',
+                    include=['tasks.scheduler',
                              'tasks.worker'])
 cel.config_from_object(config)
 logger = get_task_logger('soda')

src/config.py
@@ -20,23 +20,16 @@ CELERY_QUEUES = (
     kombu.Queue('default',
                 kombu.Exchange('default'),
                 routing_key='default'),    # >= 1 worker
-    # we have separate workers for register_request and other schedule
-    # tasks to be able to quickly respond to creation of new requests
-    # (by not blocking scheduler to block schedule_tasks and vice
-    # versa)
     kombu.Queue('scheduler',
                 kombu.Exchange('default'),
-                routing_key='schedule'),   # == 1 worker
-    kombu.Queue('registrar',
-                kombu.Exchange('default'),
-                routing_key='register')    # == 1 worker
+                routing_key='schedule')    # == 1 worker
     )
-CELERY_ROUTES = { 'tasks.registrar.register_request_demo' : { 'queue' : 'registrar' },
-                  'tasks.registrar.register_request' : { 'queue' : 'registrar' },
+CELERY_ROUTES = { 'tasks.scheduler.register_request_demo' : { 'queue' : 'scheduler' },
+                  'tasks.scheduler.register_request' : { 'queue' : 'scheduler' },
                   'tasks.scheduler.schedule_tasks' : { 'queue' : 'scheduler' },
                   'tasks.scheduler.schedule_join_staging_task' : { 'queue' : 'scheduler' },
-                  'tasks.scheduler.schedule_mark_request_deletable' : { 'queue' : 'scheduler' },
-                  'tasks.scheduler.schedule_submit_sizing_tasks' : { 'queue' : 'scheduler' } }
+                  'tasks.scheduler.schedule_mark_request_deletable' : { 'queue' : 'scheduler' } }
+
 CELERY_DEFAULT_QUEUE = 'default'
 CELERY_DEFAULT_EXCHANGE_TYPE = 'direct'
 CELERY_DEFAULT_ROUTING_KEY = 'default'

src/models.py
@@ -33,8 +33,8 @@ class DownloadRequest(db.Model):
                       nullable=False, default='created')
     is_deletable = db.Column(db.Boolean, default=False, nullable=False)
 
-    def __init__(self, openid):
-        self.uuid = uuid.uuid4().get_hex()
+    def __init__(self, uuid, openid):
+        self.uuid = uuid
         self.openid = openid
         self.time_created = datetime.utcnow()
 

src/soda.py
@@ -2,17 +2,16 @@ from __future__ import absolute_import
 import config
 import hashlib
 import logging
-import models
 import os.path
 import simplejson as json
+import tasks
+import uuid
 from collections import OrderedDict
 from flask import Flask, jsonify, redirect, request, send_from_directory, \
     url_for
-from models import db, DownloadRequest, StagableFile, Task
-from sqlalchemy import or_
+from models import db, DownloadRequest, StagableFile
 from sqlalchemy.sql import func
-from tasks.registrar import register_request, register_request_demo
-from tasks.scheduler import schedule_submit_sizing_tasks, schedule_mark_request_deletable, schedule_join_staging_task, schedule_tasks
+from tasks.scheduler import register_request, register_request_demo
 
 
 app = Flask('soda')
@@ -138,33 +137,12 @@ def http_create_request_demo():
                              'request: %s' % request, payload=str(request))
 
     file_to_query = { name : ',\n'.join('%s = %s' % (k,v) for k,v in params.iteritems()) }
-    logger.debug('=> registering new request: openid=%s, file_to_query=%s' % \
-                 (openid, file_to_query))
-
-    # the reason we run this as a worker job (which only has one
-    # worker) is to guarantee it is _not_ run concurrently. It makes
-    # it less likely we get more than one identical staging/sizing
-    # task running. We still can get orphan staging tasks if the
-    # worker crasches in the wrong place - before it's been registered
-    # in the sql db. But we don't risk each staging task being subject
-    # to the race condition that occurs in the window between checking
-    # for the existence of an existing task and starting a new task
-    # (which can lead to > 1 identical tasks being executed in a
-    # concurrent situation). This register_request task is
-    # expected to be really quick so it's not expected to be a
-    # performance issue.
-    #
-    # create request and submit sizing tasks for unknown file_names
-    r_uuid = register_request_demo.delay(openid, file_to_query).get()
-    logger.info('<= registered new request %s for openid=%s, file_to_query=%s' % \
-                (r_uuid, openid, file_to_query))
-    logger.debug('=> submitting sizing tasks for request id %s' % r_uuid)
-    schedule_submit_sizing_tasks.delay(r_uuid)
-    logger.debug('=> invoking scheduler')
-    schedule_tasks.delay()
+    r_uuid = uuid.uuid4().get_hex()
+    logger.debug('=> registering new request: uuid=%s openid=%s, '
+                 'file_to_query=%s' % (r_uuid, openid, file_to_query))
+    register_request_demo.apply_async((r_uuid, openid, file_to_query),
+                                      task_id=r_uuid)
     return jsonify(), 201, { 'location': '/request/%s' % r_uuid }
-
-
 
 
 # FIXME: requires authorization (used by the esg node)
@@ -187,31 +165,10 @@ def http_create_request():
                              payload=str(request))
     assert type(file_names) is list, file_names
     assert all(type(x) is str or type(x) is unicode for x in file_names)
-
-    logger.debug('=> registering new request: openid=%s, file_names=%s' % \
-                 (openid, file_names))
-
-    # the reason we run this as a worker job (which only has one
-    # worker) is to guarantee it is _not_ run concurrently. It makes
-    # it less likely we get more than one identical staging/sizing
-    # task running. We still can get orphan staging tasks if the
-    # worker crasches in the wrong place - before it's been registered
-    # in the sql db. But we don't risk each staging task being subject
-    # to the race condition that occurs in the window between checking
-    # for the existence of an existing task and starting a new task
-    # (which can lead to > 1 identical tasks being executed in a
-    # concurrent situation). This register_request task is
-    # expected to be really quick so it's not expected to be a
-    # performance issue.
-    #
-    # create request and submit sizing tasks for unknown file_names
-    r_uuid = register_request.delay(openid, file_names).get()
-    logger.info('<= registered new request %s for openid=%s, file_names=%s' % \
-                (r_uuid, openid, file_names))
-    logger.debug('=> submitting sizing tasks for request id %s' % r_uuid)
-    schedule_submit_sizing_tasks.delay(r_uuid)
-    logger.debug('=> invoking scheduler')
-    schedule_tasks.delay()
+    r_uuid = uuid.uuid4().get_hex()
+    logger.debug('=> registering new request: uuid=%s, openid=%s, '
+                 'file_names=%s' % (r_uuid, openid, file_names))
+    register_request.apply_async((r_uuid, openid, file_names), task_id=r_uuid)
     return jsonify(), 201, { 'location': '/request/%s' % r_uuid }
 
 
@@ -258,15 +215,11 @@ def mars():
     logger.debug('file name: %s' % file_name)
 
     file_to_query = { file_name : mars_request_text }
-    logger.debug('=> registering new request: openid=%s, file_to_query=%s' % \
-                 (openid, file_to_query))
-    r_uuid = register_request_demo.delay(openid, file_to_query).get()
-    logger.info('<= registered new request %s for openid=%s, file_to_query=%s' % \
-                (r_uuid, openid, file_to_query))
-    logger.debug('=> submitting sizing tasks for request id %s' % r_uuid)
-    schedule_submit_sizing_tasks.delay(r_uuid)
-    logger.debug('=> invoking scheduler')
-    schedule_tasks.delay()
+    r_uuid = uuid.uuid4().get_hex()
+    logger.debug('=> registering new request: uuid=%s openid=%s, '
+                 'file_to_query=%s' % (r_uuid, openid, file_to_query))
+    register_request_demo.apply_async((r_uuid, openid, file_to_query),
+                                      task_id=r_uuid)
     return jsonify(), 201, { 'location': '/request/%s' % r_uuid }
 
 
@@ -282,7 +235,17 @@ def http_delete_request():
 def http_status_request(uuid):
     #assert request.headers['Content-Type'] == 'application/json', request
     #assert 'openid' in request.json, request.json
-    r = DownloadRequest.query.get_or_404(uuid)
+    logger.debug('received request uuid %s' % uuid)
+    r = DownloadRequest.query.get(uuid)
+    if not r:
+        future = tasks.cel.AsyncResult(uuid)
+        if future.status == 'PENDING':
+            raise HTTPNotfound('found no registered request with uuid %s' % uuid)
+        return jsonify(status='unregistered',
+                       progress='0.0',
+                       staged_files=[],
+                       offline_files=[]), 200, { 'location' : '/request/%s' % uuid }
+
     all_files = set(r.files)
     staged = set(f for f in all_files if f.state == 'online')
     offline = all_files - staged
@@ -295,6 +258,7 @@ def http_status_request(uuid):
                    staged_files=urls_staged_files,
                    offline_files=urls_offline_files), 200, { 'location' : '/request/%s' % r.uuid }
 
+
 @app.route('/request/<uuid>/status/<file_name>', methods=['GET'])
 def http_status_file(uuid, file_name):
     r = DownloadRequest.query.get_or_404(uuid)
@@ -303,7 +267,6 @@ def http_status_file(uuid, file_name):
     assert r in f.requests, (r, f)
     # TODO: if is done do redirect to staged? or provide link?
     return jsonify(stdout=f.staging_task.stdout(), stderr=f.staging_task.stderr()), 200
-
 
 
 # set time of cache expiry?

src/tasks/registrar.py
@@ -1,59 +1,0 @@
-import celery
-import tasks
-from models import DownloadRequest, StagableFile, Task
-from tasks import logger
-
-
-@celery.task(acks_late=True)
-def register_request_demo(openid, file_to_query):
-    r = DownloadRequest(openid)
-    logger.debug('created new request %s' % r)
-    file_names = file_to_query.keys()
-    registered_files = tasks.session.query(StagableFile).\
-        filter(StagableFile.name.in_(file_names))
-    logger.debug('registered files: %s' % registered_files.all())
-    new_file_names = set(file_names) - set(x.name for x in registered_files)
-    logger.debug('new file names: %s' % new_file_names)
-    new_files = [ StagableFile(file_name, file_to_query[file_name]) for file_name in new_file_names ]
-    logger.debug('new files: %s' % new_files)
-    registered_files.update(
-        { StagableFile.request_count : StagableFile.request_count  + 1 },
-        synchronize_session=False)
-    r.files.extend(registered_files)
-    r.files.extend(new_files)
-    # This won't throw an integrityerror as long as this task is
-    # single threaded. If not, there's always a risk that one or more
-    # identical StagableFile may be inserted concurrently.
-    tasks.session.add(r)
-    tasks.session.commit()
-    logger.info('registered request %s for openid=%s, file_names=%s '
-                '(unregistered=%s)' % \
-                (r.uuid, openid, file_names, new_file_names))
-    return r.uuid
-
-
-@celery.task(acks_late=True)
-def register_request(openid, file_names):
-    r = DownloadRequest(openid)
-    logger.debug('created new request %s' % r)
-    registered_files = tasks.session.query(StagableFile).\
-        filter(StagableFile.name.in_(file_names))
-    logger.debug('registered files: %s' % registered_files.all())
-    new_file_names = set(file_names) - set(x.name for x in registered_files)
-    logger.debug('new file names: %s' % new_file_names)
-    new_files = [ StagableFile(file_name) for file_name in new_file_names ]
-    logger.debug('new files: %s' % new_files)
-    registered_files.update(
-        { StagableFile.request_count : StagableFile.request_count  + 1 },
-        synchronize_session=False)
-    r.files.extend(registered_files)
-    r.files.extend(new_files)
-    # This won't throw an integrityerror as long as this task is
-    # single threaded. If not, there's always a risk that one or more
-    # identical StagableFile may be inserted concurrently.
-    tasks.session.add(r)
-    tasks.session.commit()
-    logger.info('registered request %s for openid=%s, file_names=%s '
-                '(unregistered=%s)' % \
-                (r.uuid, openid, file_names, new_file_names))
-    return r.uuid

src/tasks/scheduler.py
@@ -14,6 +14,65 @@ from tasks.worker import estimate_size, stage_file
 from util import util
 
 
+@celery.task(acks_late=True, ignore_results=True)
+def register_request_demo(uuid, openid, file_to_query):
+    r = DownloadRequest(uuid, openid)
+    logger.debug('created new request %s' % r)
+    file_names = file_to_query.keys()
+    registered_files = tasks.session.query(StagableFile).\
+        filter(StagableFile.name.in_(file_names))
+    logger.debug('registered files: %s' % registered_files.all())
+    new_file_names = set(file_names) - set(x.name for x in registered_files)
+    logger.debug('new file names: %s' % new_file_names)
+    new_files = [ StagableFile(file_name, file_to_query[file_name]) for file_name in new_file_names ]
+    logger.debug('new files: %s' % new_files)
+    registered_files.update(
+        { StagableFile.request_count : StagableFile.request_count  + 1 },
+        synchronize_session=False)
+    r.files.extend(registered_files)
+    r.files.extend(new_files)
+    # This won't throw an integrityerror as long as this task is
+    # single threaded. If not, there's always a risk that one or more
+    # identical StagableFile may be inserted concurrently.
+    tasks.session.add(r)
+    tasks.session.commit()
+    logger.info('registered request %s openid=%s, file_names=%s '
+                '(unregistered=%s)' % \
+                (r.uuid, openid, file_names, new_file_names))
+    submit_sizing_tasks(r.uuid)
+    logger.debug('=> invoking scheduler')
+    schedule_tasks.delay()
+
+
+@celery.task(acks_late=True, ignore_results=True)
+def register_request(uuid, openid, file_names):
+    r = DownloadRequest(uuid, openid)
+    logger.debug('created new request %s' % r)
+    registered_files = tasks.session.query(StagableFile).\
+        filter(StagableFile.name.in_(file_names))
+    logger.debug('registered files: %s' % registered_files.all())
+    new_file_names = set(file_names) - set(x.name for x in registered_files)
+    logger.debug('new file names: %s' % new_file_names)
+    new_files = [ StagableFile(file_name) for file_name in new_file_names ]
+    logger.debug('new files: %s' % new_files)
+    registered_files.update(
+        { StagableFile.request_count : StagableFile.request_count  + 1 },
+        synchronize_session=False)
+    r.files.extend(registered_files)
+    r.files.extend(new_files)
+    # This won't throw an integrityerror as long as this task is
+    # single threaded. If not, there's always a risk that one or more
+    # identical StagableFile may be inserted concurrently.
+    tasks.session.add(r)
+    tasks.session.commit()
+    logger.info('registered request %s for openid=%s, file_names=%s '
+                '(unregistered=%s)' % \
+                (r.uuid, openid, file_names, new_file_names))
+    submit_sizing_tasks(r.uuid)
+    logger.debug('=> invoking scheduler')
+    schedule_tasks.delay()
+
+
 # raises OSError if failing to delete a file except when it does not
 # exist updates db
 def purge_files(min_size, purgable_files):
@@ -50,8 +109,7 @@ def purge_files(min_size, purgable_files):
 # file and will be updated with the actual size once the file has
 # actually been staged (every time it differs from the registered
 # value):
-@celery.task(acks_late=True, ignore_results=True)
-def schedule_submit_sizing_tasks(r_uuid):
+def submit_sizing_tasks(r_uuid):
     files_unknown_size = tasks.session.query(StagableFile).\
         join(StagableFile.requests).\
         filter(DownloadRequest.uuid == r_uuid).\

commit 818f470e8fde992dcb1dea8d7e431d04953e2234

Add explict import of celery.signals in tasks package
This regression was apparently introduced in
f3dfffea61bdb46a7334b412c05999400b668f77, but it is still unclear why.

src/tasks/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import celery
+import celery.signals
 import config
 import logging
 from celery.utils.log import get_task_logger